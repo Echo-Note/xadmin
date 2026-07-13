cc96bfb38b6d84f9ac1a7e45f045679b5be6213c
laogao <wadxf@live.com>
2026-07-13 16:28:59 +0800

    refactor: 重构应用目录结构，将各应用迁移至 apps/ 下
    - 将 captcha/common/core/system 等应用统一迁移到 server/apps/ 目录下
    - 更新所有 import 路径引用 (captcha.* -> apps.captcha.* 等)
    - 更新配置中的模块路径字符串 (CAPTCHA_CHALLENGE_FUNCT, CAPTCHA_NOISE_FUNCTIONS)
    - 更新 CAPTCHA_FONT_PATH 指向 apps/captcha/fonts/Vera.ttf
    - 更新 INSTALLED_APPS 和 URL 路由配置
    - 新增 server/apps/__init__.py

---
+++ server/apps/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2026/7/13 15:23
+# @Author  : laogao
+# @Email   : wadxf@live.com
+# @File    : __init__.py.py
+# @Update  : 2026/7/13 15:23 更新描述
